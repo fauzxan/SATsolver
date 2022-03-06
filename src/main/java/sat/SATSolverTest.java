f3c101fe30fcb7db8298a474e4fdea539d2ef237
fauzxan <92146562+fauzxan@users.noreply.github.com>
2022-03-06 18:23:43 +0800

    Update SATSolverTest.java

--- src/main/java/sat/SATSolverTest.java
+++ src/main/java/sat/SATSolverTest.java
@@ -1,14 +1,9 @@
 package sat;
-
 /*
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 */
-
-
-
-
 import immutable.ImList;
 import sat.env.*;
 import sat.formula.*;
@@ -21,59 +16,9 @@ import java.util.*;
 import java.util.regex.Pattern;
 
 public class SATSolverTest{
-    Literal a = PosLiteral.make("a");
-    Literal b = PosLiteral.make("b");
-    Literal c = PosLiteral.make("c");
-    Literal na = a.getNegation();
-    Literal nb = b.getNegation();
-    Literal nc = c.getNegation();
-
-
-
-	
-	// TODO: add the main method that reads the .cnf file and calls SATSolver.solve to determine the satisfiability
-    
-	
-    public void testSATSolver1(){
-    	// (a v b)
-    	Environment e = SATSolver.solve(makeFm(makeCl(a,b))	);
-/*
-    	assertTrue( "one of the literals should be set to true",
-    			Bool.TRUE == e.get(a.getVariable())  
-    			|| Bool.TRUE == e.get(b.getVariable())	);
-    	
-*/    	
-    }
-    
-    
-    public void testSATSolver2(){
-    	// (~a)
-    	Environment e = SATSolver.solve(makeFm(makeCl(na)));
-/*
-    	assertEquals( Bool.FALSE, e.get(na.getVariable()));
-*/    	
-    }
-    
-    private static Formula makeFm(Clause... e) {
-        Formula f = new Formula();
-        for (Clause c : e) {
-            f = f.addClause(c);
-        }
-        return f;
-    }
-    
-    private static Clause makeCl(Literal... e) {
-        Clause c = new Clause();
-        for (Literal l : e) {
-            c = c.add(l);
-        }
-        return c;
-    }
-
     public static void main(String[] args) throws FileNotFoundException {
         String Path =args[0];
         System.out.println("\nFile location: "+Path+"\n");
-
         Formula f=new Formula();
         long parsingstart= System.nanoTime();
         try{
@@ -92,7 +37,7 @@ public class SATSolverTest{
                 }
                 if (line.length() == 0) {
                     continue;
-                }// if its an empty line it should increment flag
+                }
                 else if (!pattern.matcher(line_split[0]).matches()) {// to check if the line contains any non integer values
                     continue;
                 }
@@ -125,7 +70,6 @@ public class SATSolverTest{
         long started=System.nanoTime();
         //System.out.println("\nThe logic reaches this point");
         Environment result=solver.solve(f);
-
         long ended=System.nanoTime();
         long time_taken=ended-started;
         System.out.println("\nTime taken: "+ time_taken/Math.pow(10.0,9.0) + "s");
@@ -136,15 +80,13 @@ public class SATSolverTest{
                 FileWriter writer=new FileWriter(somename);
                 writer.write("The equation is unsatisfiable");
                 writer.write("\nThe runtime of the parsing process was: "+ parsingtime +" ns");
-                writer.write("\nThe runnning time of the solcing process was: "+ time_taken+ "ns");
+                writer.write("\nThe running time of the solving process was: "+ time_taken+ "ns");
                 writer.close();
             }
             catch (Exception e){
                 System.out.println("\nError while writing to BoolAssignment.txt. Equation is unsatisfiable btw");
                 e.getStackTrace();
             }
-
-
         }
         else{
             System.out.println("Satisfiable");
@@ -160,7 +102,7 @@ public class SATSolverTest{
                 }
                 writer.write("\nThe equation is satisfiable\n");
                 writer.write("\nThe runtime of the parsing process was: "+ parsingtime+ "ns\n");
-                writer.write("\nThe runnning time of the solcing process was: "+ time_taken+ "ns\n");
+                writer.write("\nThe running time of the solving process was: "+ time_taken+ "ns\n");
                 writer.write("\nVariable assignments if you really have the time to go through this lmao\n\n");
                 writer.write(result.toString());
                 writer.close();
@@ -170,6 +112,5 @@ public class SATSolverTest{
                 e.getStackTrace();
             }
         }
-
     }
 }
